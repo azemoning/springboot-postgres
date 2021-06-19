ee64843b561a54c7577e04c9843631e96155da9b
azemoning <ahmadsaufimaulana@gmail.com>
2021-06-19 09:56:11 +0700

    initial commit

---
+++ src/main/java/dev/azemoning/springbootpostgres/SpringbootPostgresApplication.java
@@ -0,0 +1,13 @@
+package dev.azemoning.springbootpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPostgresApplication.class, args);
+	}
+
+}
